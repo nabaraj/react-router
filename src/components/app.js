95d5d87697e0fb2fe319c93d97d82a4fff82c1ad
SAPIENT\nsaha1 <nsaha2@sapient.com>
2017-12-09 19:49:18 +0530

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
---
+++ src/components/posts_new.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+class PostsNew extends Component{
+  render(){
+    return (<div>Create form</div>);
+  }
+};
+
+export default PostsNew;
